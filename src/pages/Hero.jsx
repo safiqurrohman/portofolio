a33930d6f16311c272f28f5cd380cee9d5457258
safik <safikur828@gmail.com>
2026-03-15 22:38:31 +0700

    nambahin link CV

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -22,7 +22,7 @@ const Hero = () => {
                         yang fungsional.</p>
                     <div className="flex sm:gap-4 gap-2 items-center">
                         <a
-                            href="#"
+                            href="https://drive.google.com/file/d/1gg4F0ahSDJ2-hfNqi44wBg6pXMMEk2S6/view?usp=sharing"
                             className='h-12 px-8 rounded-lg bg-violet-700 hover:bg-violet-500 text-white font-semibold transition-all shadow-[0_0_20px_rgba(124,58,237,0.3)] hover:shadow-[0_0_25px_rgba(124,58,237,0.5)] hover:-translate-y-0.5 flex items-center justify-center gap-2 group border border-primary-light/20'>Download CV <i className="ri-download-line ri-lg"></i></a>
                         <a
                             href="#proyek"
